583e5f1eb55772900de433c35a4f35da7b3d3a4b
bhscs-build <bhs.cs.build@gmail.com>
2018-10-08 12:18:55 -0700

    Populating starter project: Part 2

---
+++ StacksQueues/src/Deque.java
@@ -0,0 +1,44 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item>
+{
+    public Deque()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public boolean isEmpty()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public int size()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void addFirst(Item item)
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void addLast(Item item)
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public Item removeFirst()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public Item removeLast()
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    public Iterator<Item> iterator()
+    {
+        throw new UnsupportedOperationException();
+    }
+ }
